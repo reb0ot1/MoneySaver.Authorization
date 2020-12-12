5833982ff2a052efeb4c724bbb13d30f09470a41
sbozhinov <sbojinov@yahoo.com>
2020-12-12 14:20:57 +0200

    Initial setup

--- MoneySaver.Identity.Api/Config.cs
+++ MoneySaver.Identity.Api/Config.cs
@@ -1,8 +1,4 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-
-using IdentityServer4.Models;
+﻿using IdentityServer4.Models;
 using System.Collections.Generic;
 
 namespace IdentityServerAspNetIdentity
@@ -12,46 +8,53 @@ namespace IdentityServerAspNetIdentity
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
                    {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
+                        new IdentityResources.OpenId(),
+                        new IdentityResources.Profile(),
                    };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
             {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
+                new ApiScope("manage", "manage api")
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {
+                new ApiResource("moneysaverapi", "Money saver api")
+                { 
+                    Scopes = { "manage"}
+                }
             };
 
         public static IEnumerable<Client> Clients =>
             new Client[]
             {
-                // m2m client credentials flow client
+                //new Client
+                //{
+                //    ClientId = "moneysaverapiclient",
+                //    ClientName = "Money Saver API client",
+                //    //RedirectUris = { "http://localhost:6001" },
+                //    //FrontChannelLogoutUri = "http://activities.moneysaver.local/signout-oidc",
+                //    //PostLogoutRedirectUris = { "http://activities.moneysaver.local/signout-callback-oidc" },
+                //    AllowAccessTokensViaBrowser = true,
+                //    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                //    ClientSecrets = { new Secret("moneysaverapikey".Sha256()) },
+
+                //    AllowedScopes = { "openid", "profile", "moneysaverapi2" }
+                //},
+
                 new Client
                 {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
-
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "scope1" }
-                },
-
-                // interactive client using code flow + pkce
-                new Client
-                {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
+                    ClientId = "blazor",
                     AllowedGrantTypes = GrantTypes.Code,
-
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+                    AllowedCorsOrigins = { "https://localhost:4001" },
+                    AllowedScopes = { "openid", "profile", "manage" },
+                    RedirectUris = { "https://localhost:4001/authentication/login-callback" },
+                    PostLogoutRedirectUris = { "https://localhost:4001/" },
+                    Enabled = true
                 },
             };
     }
